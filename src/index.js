c0c84f573a229677dcb03ead691458bf80f020ef
Vyacheslav Terskikh <troxa843@gmail.com>
2023-06-09 08:15:29 +0300

    Refactor result of gendiff

--- src/index.js
+++ src/index.js
@@ -1,28 +1,24 @@
 import _ from 'lodash';
-import { readFile } from './utils.js';
+import readFile from './utils.js';
 
 const genDiff = (file1, file2) => {
   const object1 = readFile(file1);
   const object2 = readFile(file2);
   const unitedKeys = _.union(_.keys(object1), _.keys(object2));
   const sortedKeys = _.sortBy(unitedKeys);
-  const lines = [];
-  lines.push('{\n');
-  for (const key of sortedKeys) {
+  const lines = sortedKeys.map((key) => {
     if (!_.has(object2, key)) {
-      lines.push(`\t- ${key}: ${object1[key]}\n`);
-    } else if (!_.has(object1, key)) {
-      lines.push(`\t+ ${key}: ${object2[key]}\n`);
-    } else if (object1[key] !== object2[key]) {
-      lines.push(`\t- ${key}: ${object1[key]}\n`);
-      lines.push(`\t+ ${key}: ${object2[key]}\n`);
-    } else {
-      lines.push(`\t  ${key}: ${object1[key]}\n`);
+      return `\t- ${key}: ${object1[key]}\n`;
     }
-  }
-  lines.push('}');
-  const diff = lines.join('');
-  return diff;
+    if (!_.has(object1, key)) {
+      return `\t+ ${key}: ${object2[key]}\n`;
+    }
+    if (object1[key] !== object2[key]) {
+      return `\t- ${key}: ${object1[key]}\n\t+ ${key}: ${object2[key]}\n`;
+    }
+    return `\t  ${key}: ${object1[key]}\n`;
+  });
+  return ['{\n', ...lines, '}'].join('');
 };
 
 export default genDiff;
